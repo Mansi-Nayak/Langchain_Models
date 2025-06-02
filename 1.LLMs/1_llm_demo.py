a768c630d7a03c40f2a149f5028f42072df9d5be
Mansi Nayak <nayak.mansi09@gmail.com>
2025-06-02 17:57:07 +0530

    Langchai Close source Openai

---
+++ 1.LLMs/1_llm_demo.py
@@ -0,0 +1,17 @@
+"""
+For API key you need to visit platform.openai.com, It is a close source model 
+so it is paid. model is in company server we can communicate only through api 
+only so we don't have control to make changes.
+"""
+
+from langchain_openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = OpenAI(model='gpt-3.5-turbo-instruct')
+
+result = llm.invoke("What is the capital of India")
+
+
+print(result)
