a23bc5bcf060f9c0ffa030874e99dbd05ae02c71
Mansi Nayak <nayak.mansi09@gmail.com>
2025-06-03 17:48:59 +0530

    Embedding Model document similarity

--- 3.EmbeddedModels/4_document_simlarity.py
+++ 3.EmbeddedModels/4_document_simlarity.py
@@ -1,20 +1,20 @@
 """
-Let say we have 5 docs now user will one question and the question is related 
-one of the document among the 5 document. We need to check from which document 
-the question belong.solve = generate the embedding of 5 docs as well as 
-generate the embeddings of the question so basically embedding is vectors so 
-here 5 vectors and 1 vector. In 300dm we have 5 vectors a new vector came we 
-need to find out that 1 vector is closed to which of the among 5 vector whose 
+Let say we have 5 docs now user will one question and the question is related
+one of the document among the 5 document. We need to check from which document
+the question belong.solve = generate the embedding of 5 docs as well as
+generate the embeddings of the question so basically embedding is vectors so
+here 5 vectors and 1 vector. In 300dm we have 5 vectors a new vector came we
+need to find out that 1 vector is closed to which of the among 5 vector whose
 similarity score is more is the answer.
 """
 
-from langchain_openai import OpenAIEmbeddings
 from dotenv import load_dotenv
+from langchain_openai import OpenAIEmbeddings
 from sklearn.metrics.pairwise import cosine_similarity
 
 load_dotenv()
 
-embedding = OpenAIEmbeddings(model='text-embedding-3-large', dimensions=300)
+embedding = OpenAIEmbeddings(model="text-embedding-3-large", dimensions=300)
 
 documents = [
     "Virat Kohli is a modern-day batting great known for his aggressive style and consistency across all formats."
@@ -35,7 +35,7 @@ query_embedding = embedding.embed_query(query)
 
 scores = cosine_similarity([query_embedding], doc_embeddings)[0]
 
-index, score = sorted(list(enumerate(scores)), key=lambda x:x[1])[-1]
+index, score = sorted(list(enumerate(scores)), key=lambda x: x[1])[-1]
 
 print(query)
 print(documents[index])
