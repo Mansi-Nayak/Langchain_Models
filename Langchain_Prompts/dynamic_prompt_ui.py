fefd25ff294ed9bd5eae1dac67dbc37814a5698d
Mansi Nayak <nayak.mansi09@gmail.com>
2025-06-03 18:01:54 +0530

    add dynamic prompt ui using streamlit

---
+++ Langchain_Prompts/dynamic_prompt_ui.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+
+model = ChatOpenAI()
+
+st.header("🧠 Research Tool")
+
+# Dynamic
+
+paper_input = st.selectbox(
+    "Select Research Paper Name",
+    [
+        "Attention Is All You Need",
+        "BERT: Pre-training of Deep Bidirectional Transformers",
+        "GPT-3: Language Models are Few-Shot Learners",
+        "Diffusion Models Beat GANs on Image Synthesis",
+    ],
+)
+
+style_input = st.selectbox(
+    "Select Explanation Style",
+    ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"],
+)
+
+length_input = st.selectbox(
+    "Select Explanation Length",
+    [
+        "Short (1-2 paragraphs)",
+        "Medium (3-5 paragraphs)",
+        "Long (detailed explanation)",
+    ],
+)
+
+
+if st.button("Summarize"):
+    print("Hello")
