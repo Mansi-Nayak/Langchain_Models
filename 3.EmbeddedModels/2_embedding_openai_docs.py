5ab802957cc4b2b488ba23f01cd4e0c8155597bf
Mansi Nayak <nayak.mansi09@gmail.com>
2025-06-03 17:47:13 +0530

    Embedding Model close source Openai docs

--- 3.EmbeddedModels/2_embedding_openai_docs.py
+++ 3.EmbeddedModels/2_embedding_openai_docs.py
@@ -1,14 +1,14 @@
-from langchain_openai import OpenAIEmbeddings
 from dotenv import load_dotenv
+from langchain_openai import OpenAIEmbeddings
 
 load_dotenv()
 
-embedding = OpenAIEmbeddings(model='text-embedding-3-large', dimensions=32)
+embedding = OpenAIEmbeddings(model="text-embedding-3-large", dimensions=32)
 
 documents = [
     "Delhi is the capital of India",
     "Kolkata is the capital of West Bengal",
-    "Paris is the capital of France"
+    "Paris is the capital of France",
 ]
 
 result = embedding.embed_documents(documents)
