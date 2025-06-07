ef1da034b9c7f9656a9352d26a416098ae4053b7
Mansi Nayak <nayak.mansi09@gmail.com>
2025-06-07 20:14:06 +0530

    add static prompt ui using streamlit

--- Langchain_Prompts/static_prompt_ui.py
+++ Langchain_Prompts/static_prompt_ui.py
@@ -1,3 +1,16 @@
+"""
+1. It uses Streamlit to build a web interface and loads environment variables
+   using load_dotenv().
+2. It initializes an OpenAI language model (gpt-3.5-turbo) using LangChain with
+   a creativity setting (temperature=0.7).
+3. A text input box collects the user’s prompt, and a button labeled
+   "Summarize" triggers the model.
+4. When clicked, the model generates a response based on the prompt using
+   model.invoke().
+5. The generated summary is then displayed using st.write(), or a warning is
+   shown if input is empty.
+"""
+
 import streamlit as st
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
@@ -6,7 +19,7 @@ load_dotenv()
 
 model = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.7)
 
-st.header("🧠 Research Tool - Summarize Anything")
+st.header("Research Tool - Summarize Anything")
 
 # static
 user_input = st.text_input("Enter your prompt")
