31ed605c0d52738e4bd3acaa3743fb5e7fd0515f
Mansi Nayak <nayak.mansi09@gmail.com>
2025-06-17 21:54:53 +0530

    Refactor prompt template for improved clarity and reusability

--- Langchain_Prompts/chat_prompt_template.py
+++ Langchain_Prompts/chat_prompt_template.py
@@ -1,18 +1,20 @@
 """
 system message:- dynamic
                 [you are a helpful [domain] expect]
-Human message:- 
+Human message:-
                 explain about [topic]
 ChatPromptTemplate = create a dynamic templates.
 """
 
 from langchain_core.prompts import ChatPromptTemplate
 
-chat_template = ChatPromptTemplate([
-    ('system', 'You are a helpful {domain} expert'),
-    ('human', 'Explain in simple terms, what is {topic}')
-])
+chat_template = ChatPromptTemplate(
+    [
+        ("system", "You are a helpful {domain} expert"),
+        ("human", "Explain in simple terms, what is {topic}"),
+    ]
+)
 
-prompt = chat_template.invoke({'domain': 'cricket', 'topic': 'Dusra'})
+prompt = chat_template.invoke({"domain": "cricket", "topic": "Dusra"})
 
 print(prompt)
